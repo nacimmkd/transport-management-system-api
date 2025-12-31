2bb2cb45d538907704656251e85f2ac3a2e6b3b6
nacimmkd <nacimmakedhi@gmail.com>
2025-12-31 18:16:22 +0100

    added Email templates

--- src/main/java/com/tms/services/test/testService.java
+++ src/main/java/com/tms/services/test/testService.java
@@ -1,16 +1,20 @@
 package com.tms.services.test;
 
 import com.tms.services.NotificationService.EmailNotificationService;
-import lombok.AllArgsConstructor;
+import com.tms.services.NotificationService.EmailTemplates;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.SecondaryRow;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class testService {
 
-    private final EmailNotificationService notificationService;
+    private final EmailNotificationService emailNotificationService;
 
     public void sendEmail() {
-        notificationService.sendEmail("nacimmakedhi@gmail.com", "Test email", "this is an email of test");
+        var template = EmailTemplates.getEmailCredential("Nadjib" , "nassimmakdi2001@gmail.com","123");
+        emailNotificationService.sendEmail("nassimmakdi2001@gmail.com","Email de Test", template);
     }
+
 }
